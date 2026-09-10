1ebe620df738ff624b516d2ea5c50f4d9f425cb1
Guilherme <guigalopes741@gmail.com>
2026-09-10 01:37:07 -0300

    removed debugging codes

--- Src/Transformer/TrainTransformer.py
+++ Src/Transformer/TrainTransformer.py
@@ -1,7 +1,3 @@
-import os
-
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-import torch
 import torch
 from Src.Transformer.DatasetPreLoader import DataLoaderPackerLocal
 from Src.Transformer.Optimizer.AdamOpimizer import AdamOptimizer
