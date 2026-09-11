705fd32dee8783e5cb63a6adfe0e9662fee15f4c
Guilherme <guigalopes741@gmail.com>
2026-09-11 00:14:15 -0300

    Minor changes

--- Src/Transformer/TrainTransformer.py
+++ Src/Transformer/TrainTransformer.py
@@ -5,9 +5,9 @@ from Src.Transformer.Settings.Config import DEVICE
 from Src.Transformer.Transformer import Transformer
 from tqdm import tqdm
 
-transformer = Transformer(epochs=2, batch_size=16, d_model=512, vocab_size=10259, causal_mask_size=2048, max_seq_len=256, num_blocks=6)
+transformer = Transformer(epochs=1, batch_size=16, d_model=512, vocab_size=10259, causal_mask_size=2048, max_seq_len=256, num_blocks=6)
 transformer.to(DEVICE)
-dataloader = DataLoaderPackerLocal(base_path=f"/kaggle/working/DatasetAi", max_parts=5, seq_len=256, batch_size=16)
+dataloader = DataLoaderPackerLocal(base_path=f"/kaggle/working/DatasetAi", max_parts=20, seq_len=256, batch_size=16)
 total_steps = dataloader.get_max_steps() * transformer.epochs
 optimizer = AdamOptimizer(parameters=transformer.get_params(), alpha=3e-4, total_steps=total_steps)
 
