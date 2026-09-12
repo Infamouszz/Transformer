b32a9f81c65d49006d3750c322c401daaed21a9c
Guilherme <guigalopes741@gmail.com>
2026-09-11 23:10:05 -0300

    Full train mode

--- Src/Transformer/Inference.py
+++ Src/Transformer/Inference.py
@@ -11,7 +11,7 @@ tokenizer_instance = Tokenizer.BPETokenizer(merges_file=path, pattern=pattern)
 transformer_instance = Transformer(epochs=1, batch_size=1, d_model=512, vocab_size=10259, causal_mask_size=2048, max_seq_len=256, num_blocks=6)
 transformer_instance.load_params(r"E:/ModelAi/parameters.pt")
 
-def generate_text(transformer, tokens_tensor, max_tokens_generated, eos_id, temperature=0.2, repetition_penalty=1.2):
+def generate_text(transformer, tokens_tensor, max_tokens_generated, eos_id, temperature=0.6, repetition_penalty=1.2):
     with torch.no_grad():
         current_tokens = tokens_tensor.clone()
 
@@ -39,7 +39,7 @@ def generate_text(transformer, tokens_tensor, max_tokens_generated, eos_id, temp
 
     return current_tokens
 
-text = "Se eu soubesse eu teria"
+text = "Olá "
 tokens = tokenizer_instance.encode(text)
 seq_len = len(tokens)
 tensor_tokens = torch.tensor([tokens]).to(DEVICE)
