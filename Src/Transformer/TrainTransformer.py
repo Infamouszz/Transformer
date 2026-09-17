0bf1861dc88083da16f3e2c153469175f803a79a
Guilherme <guigalopes741@gmail.com>
2026-09-17 16:03:51 -0300

    Increased epoch for fit in smaller dataset

--- Src/Transformer/TrainTransformer.py
+++ Src/Transformer/TrainTransformer.py
@@ -4,7 +4,7 @@ from Src.Transformer.Optimizer.AdamOpimizer import AdamOptimizer
 from Src.Transformer.Settings.Config import DEVICE
 from Src.Transformer.Transformer import Transformer
 
-transformer = Transformer(epochs=2, batch_size=16, d_model=512, vocab_size=10263, causal_mask_size=2048, max_seq_len=512, num_blocks=6)
+transformer = Transformer(epochs=4, batch_size=16, d_model=512, vocab_size=10263, causal_mask_size=2048, max_seq_len=512, num_blocks=6)
 transformer.to(DEVICE)
 dataloader = DataLoaderPackerLocal(base_path=f"/kaggle/working/DatasetAiSFT_FINAL", max_parts=15, seq_len=512, batch_size=1)
 total_steps = dataloader.get_max_steps() * transformer.epochs
